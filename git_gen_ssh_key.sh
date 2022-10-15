52bc47f0b378ef9b98ad28e3dcb574186e94198f
GabrielTMartinez <lightwanderer@DESKTOP-JOT4TP5.localdomain>
2022-10-15 18:06:34 -0300

    Adding helper messages and colors to git_gen_ssh_key.sh

--- git_gen_ssh_key.sh
+++ git_gen_ssh_key.sh
@@ -4,4 +4,7 @@ ssh-keygen -t ed25519 -C "GabrielTMartinez"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
-echo "You should visit https://github.com/settings/keys and add your ~/.ssh/id_ed25519.pub there"
+CYAN='\033[0;36m'
+NC='\033[0m'
+printf "\nYou should visit ${CYAN}https://github.com/settings/keys${NC} and add your ~/.ssh/id_ed25519.pub there"
+printf "\nVisit ${CYAN}https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints${NC} to verify GitHubs ssh fingerprints\n"
