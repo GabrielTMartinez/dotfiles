645c0152898d684db84645ab48dd95bcdcbda8e4
GabrielTMartinez <lightwanderer@DESKTOP-JOT4TP5.localdomain>
2022-10-15 17:59:13 -0300

    Adding message remembering to add pub key to github

--- git_gen_ssh_key.sh
+++ git_gen_ssh_key.sh
@@ -4,3 +4,4 @@ ssh-keygen -t ed25519 -C "GabrielTMartinez"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
+echo "You should visit https://github.com/settings/keys and add your ~/.ssh/id_ed25519.pub there"
