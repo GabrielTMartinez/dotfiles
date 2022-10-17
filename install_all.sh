b9dfd574745879c78ad2f374d02ffb4eec8cd6de
GabrielTMartinez <lightwanderer@DESKTOP-JOT4TP5.localdomain>
2022-10-16 21:51:41 -0300

    One script to rule them all

---
+++ install_all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+source ./install_brew.sh
+source ./install_pyenv.sh
+source ./install_colorscheme_vim.sh
+source ./setup_dotfiles.sh
+source ./install_sdkman.sh
+source ./setup_wsl.sh
+
