f4af22e770e9095929f6594f5d09ec7f8a6a8de5
GabrielTMartinez <lightwanderer@DESKTOP-JOT4TP5.localdomain>
2022-10-20 23:40:24 -0300

    Adding Jedi script to all scripts runner

--- install_all.sh
+++ install_all.sh
@@ -8,6 +8,7 @@ sudo apt -y autoremove
 source ./install_brew.sh
 source ./install_pyenv.sh
 source ./install_colorscheme_vim.sh
+source ./install_jedi_python_vim.sh
 source ./setup_dotfiles.sh
 source ./install_zsh.sh
 source ./install_sdkman.sh
