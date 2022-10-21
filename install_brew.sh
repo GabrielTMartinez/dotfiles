979d36a0a7ee8f586ab66a8060a6034c4c037182
GabrielTMartinez <lightwanderer@DESKTOP-JOT4TP5.localdomain>
2022-10-21 01:14:22 -0300

    Correcting scripts

--- install_brew.sh
+++ install_brew.sh
@@ -6,11 +6,11 @@ zprofile="$HOME/.zprofile"
 profiles="$profile $zprofile"
 
 # install, config and update homebrew
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo '' >> $profiles
-echo '# Homebrew' >> $profiles
-echo '# Set PATH, MANPATH, etc., for Homebrew.' >> $profiles
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $profiles
+NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '' | tee -a $profiles > /dev/null
+echo '# Homebrew' | tee -a $profiles > /dev/null
+echo '# Set PATH, MANPATH, etc., for Homebrew.' | tee -a $profiles > /dev/null
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' | tee -a $profiles > /dev/null
 
 source $profile
 source $zprofile
--- install_zsh.sh
+++ install_zsh.sh
@@ -9,10 +9,11 @@ zsh --version
 chsh -s $(which zsh)
 
 # install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 # source the aliases from bash
-source $HOME/.bash_aliases
+echo 'source $HOME/.bash_aliases' >> $HOME/.zshrc
 
 # fonts that enable some symbols - used for powerlevel10k
 sudo apt -y install fonts-powerline
@@ -26,7 +27,9 @@ echo ''
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # set powerlevel10k as the default zsh theme
-theme "powerlevel10k/powerlevel10k"
+sed -i 's/plugins=\([a-zA-Z ]*/& theme/' $HOME/.zshrc
+zsh -c 'source $HOME/.zshrc'
+zsh -c 'theme "powerlevel10k/powerlevel10k"'
 
 # check if file exists
 #if [ ! -f ~/.zshrc ]; then
