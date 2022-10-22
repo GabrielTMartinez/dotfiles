98b4913a65cee773e6fd19bba60841002868eb0b
GabrielTMartinez <lightwanderer@DESKTOP-JOT4TP5.localdomain>
2022-10-22 13:58:38 -0300

    Adjusting nvm install

--- install_nvm.sh
+++ install_nvm.sh
@@ -2,17 +2,14 @@
 set -e
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | zsh
 
 source $HOME/.nvm/nvm.sh
 
 nvm install --lts
 nvm use --lts
 
-#echo '' | tee -a $HOME/.bashrc $HOME/.zshrc > /dev/null
-#echo '# NVM' | tee -a $HOME/.bashrc $HOME/.zshrc > /dev/null
-#echo 'export NVM_DIR="$HOME/.nvm"' | tee -a $HOME/.bashrc $HOME/.zshrc > /dev/null
-#echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' | tee -a $HOME/.bashrc $HOME/.zshrc > /dev/null
-#echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' | tee -a $HOME/.bashrc $HOME/.zshrc > /dev/null
+echo '' | tee -a $HOME/.zshrc > /dev/null
+echo '# NVM' | tee -a $HOME/.zshrc > /dev/null
+echo 'source $HOME/.nvm/nvm.sh' | tee -a $HOME/.zshrc > /dev/null
 echo 'nvm use node > /dev/null' | tee -a $HOME/.bashrc $HOME/.zshrc > /dev/null
 
