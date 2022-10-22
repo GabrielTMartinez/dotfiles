77a5679e94dc35d6b95f9428d60fa40b204b50e6
GabrielTMartinez <lightwanderer@DESKTOP-JOT4TP5.localdomain>
2022-10-22 00:17:31 -0300

    Adjusting build dependencies for python

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -25,5 +25,8 @@ source $profile
 source $zprofile
 
 # install pyenv dependencies, used for installing python from source
-sudo apt install zlib1g-dev
+sudo apt -y install build-essential gdb lcov pkg-config \
+libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev \
+libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev \
+lzma lzma-dev tk-dev uuid-dev zlib1g-dev
 
