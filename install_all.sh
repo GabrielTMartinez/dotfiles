43618504e0996167449fcb6ed4e2094e4f4df70b
GabrielTMartinez <lightwanderer@DESKTOP-JOT4TP5.localdomain>
2022-10-22 01:50:05 -0300

    Adding vim back to the game

--- install_all.sh
+++ install_all.sh
@@ -5,13 +5,14 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y autoremove
 
-source ./install_brew.sh
-source ./install_pyenv.sh
+source ./install_vim.sh
 source ./install_colorscheme_vim.sh
 # source ./install_jedi_python_vim.sh
 source ./install_nvm.sh
 source ./install_coc.nvim.sh
 source ./setup_dotfiles.sh
+source ./install_brew.sh
+source ./install_pyenv.sh
 source ./install_zsh.sh
 source ./install_sdkman.sh
 source ./setup_wsl.sh
