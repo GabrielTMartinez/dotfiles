5ea335a12d05a1d5b17e05dd3534138558e84a56
GabrielTMartinez <lightwanderer@DESKTOP-JOT4TP5.localdomain>
2022-10-22 14:37:47 -0300

    Adding nvm plugin to oh my zsh

--- install_nvm.sh
+++ install_nvm.sh
@@ -8,8 +8,11 @@ source $HOME/.nvm/nvm.sh
 nvm install --lts
 nvm use --lts
 
-echo '' | tee -a $HOME/.zshrc > /dev/null
-echo '# NVM' | tee -a $HOME/.zshrc > /dev/null
-echo 'source $HOME/.nvm/nvm.sh' | tee -a $HOME/.zshrc > /dev/null
+#echo '' | tee -a $HOME/.zshrc > /dev/null
+#echo '# NVM' | tee -a $HOME/.zshrc > /dev/null
+#echo 'source $HOME/.nvm/nvm.sh' | tee -a $HOME/.zshrc > /dev/null
+
+sed -r -i 's/plugins=\([a-zA-Z ]*/& nvm/' $HOME/.zshrc
+
 echo 'nvm use node > /dev/null' | tee -a $HOME/.bashrc $HOME/.zshrc > /dev/null
 
