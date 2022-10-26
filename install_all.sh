d492cc646639db7df1528183072a6254b0e9f510
GabrielTMartinez <lightwanderer@DESKTOP-JOT4TP5.localdomain>
2022-10-26 00:39:45 -0300

    Moving installs files, renaming them and adjusting install_all

--- install_all.sh
+++ install_all.sh
@@ -5,16 +5,21 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y autoremove
 
-source ./install_zsh.sh
-source ./install_vim.sh
-source ./install_colorscheme_vim.sh
-# source ./install_jedi_python_vim.sh
-source ./install_nvm.sh
-source ./install_coc.nvim.sh
-# source ./install_pyright.sh # run in the beggining of a project
+current_dir=$pwd
+cd ./install_files
+
+source ./zsh.sh
+source ./vim.sh
+source ./colorscheme_vim.sh
+# source ./jedi_python_vim.sh
+source ./nvm.sh
+source ./coc.nvim.sh
+# source ./pyright.sh # run in the beggining of a project
 source ./setup_dotfiles.sh
-source ./install_brew.sh
-source ./install_pyenv.sh
-source ./install_sdkman.sh
+source ./brew.sh
+source ./pyenv.sh
+source ./sdkman.sh
 source ./setup_wsl.sh
+
+cd $current_dir
 
