af5167fac68c4594ecd4d4291fa0d90e3b03834c
GabrielTMartinez <lightwanderer@DESKTOP-JOT4TP5.localdomain>
2022-10-27 22:22:26 -0300

    Adjusting install all

--- install_all.sh
+++ install_all.sh
@@ -7,19 +7,15 @@ sudo apt -y autoremove
 
 current_dir=$pwd
 cd ./install_files
-
 source ./zsh.sh
 source ./vim.sh
-source ./colorscheme_vim.sh
-# source ./jedi_python_vim.sh
+source ./vim_plugins.sh
 source ./nvm.sh
-source ./coc.nvim.sh
-# source ./pyright.sh # run in the beggining of a project
-source ./setup_dotfiles.sh
 source ./brew.sh
 source ./pyenv.sh
 source ./sdkman.sh
-source ./setup_wsl.sh
-
 cd $current_dir
+
+source ./setup_dotfiles.sh
+source ./setup_wsl.sh
 
