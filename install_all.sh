902e84cb2c68f78f7ebbb871b37397509c298c50
GabrielTMartinez <gabrieltmartinez@DESKTOP-JOT4TP5.localdomain>
2022-11-02 22:52:56 -0300

    changing bash var name to avoid collission

--- install_all.sh
+++ install_all.sh
@@ -5,7 +5,7 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y autoremove
 
-current_dir=$(pwd)
+all_current_dir=$(pwd)
 cd ./install_files
 source ./zsh.sh
 source ./vim.sh
@@ -14,7 +14,7 @@ source ./vim_plugins.sh
 source ./brew.sh
 source ./pyenv.sh
 source ./sdkman.sh
-cd $current_dir
+cd $all_current_dir
 
 source ./setup_dotfiles.sh
 source ./setup_wsl.sh
