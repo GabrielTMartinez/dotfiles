96c2a7ccd504dab191ee21f2d601570810e36548
GabrielTMartinez <gabrieltmartinez@DESKTOP-JOT4TP5.localdomain>
2022-11-02 23:47:18 -0300

    adding ale and commenting pyright and coc

--- install_files/vim_plugins.sh
+++ install_files/vim_plugins.sh
@@ -2,11 +2,11 @@
 set -e
 
 # coc.nvim
-nvm use node
+#nvm use node
 
-mkdir -p ~/.vim/pack/coc/start
-git clone --branch release https://github.com/neoclide/coc.nvim.git --depth=1 ~/.vim/pack/coc/start/coc.nvim
-vim -c "helptags ~/.vim/pack/coc/start/coc.nvim/doc/ | qall"
+#mkdir -p ~/.vim/pack/coc/start
+#git clone --branch release https://github.com/neoclide/coc.nvim.git --depth=1 ~/.vim/pack/coc/start/coc.nvim
+#vim -c "helptags ~/.vim/pack/coc/start/coc.nvim/doc/ | qall"
 
 # gruvbox
 $(git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox)
@@ -15,9 +15,13 @@ $(git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gru
 #$(git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/pack/jedi/start/jedi-vim)
 
 # pyright
-nvm use node
-vim -c ':CocInstall coc-pyright | qall'
+#nvm use node
+#vim -c ':CocInstall coc-pyright | qall'
 
 # vim-airline
 $(git clone https://github.com/vim-airline/vim-airline.git ~/.vim/pack/default/start/vim-airline)
+
+# ale
+mkdir -p ~/.vim/pack/git-plugins/start
+git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
 
