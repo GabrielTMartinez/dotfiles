9d853cbd916f49620d0918c8b9990e1653b6aba7
GabrielTMartinez <GabrielTMartinez>
2023-01-27 14:35:36 -0300

    adjustments

--- install_files/zsh.sh
+++ install_files/zsh.sh
@@ -5,4 +5,14 @@ set -e
 echo "Installing zsh..."
 sudo apt -y install zsh
 zsh --version
+
+echo '
+HISTSIZE=5000               #How many lines of history to keep in memory
+HISTFILE=$HOME/.zsh_history #Where to save history to disk
+SAVEHIST=5000               #Number of history entries to save to disk
+#HISTDUP=erase               #Erase duplicates in the history file
+setopt    appendhistory     #Append history to the history file (no overwriting)
+setopt    sharehistory      #Share history across terminals
+setopt    incappendhistory  #Immediately append to the history file, not just when a term is killed
+' >> $HOME/.zshrc
 echo "Installing zsh... Done!"
