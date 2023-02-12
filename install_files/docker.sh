743697f436ec188be058e790adf8e85ed659861e
GabrielTMartinez <email@email.com>
2023-02-12 01:20:24 -0300

    adjustments

--- install_files/docker.sh
+++ install_files/docker.sh
@@ -4,17 +4,25 @@ set -e
 
 echo "Installing docker..."
 
-sudo apt -y update
-sudo apt -y install ca-certificates curl gnupg lsb-release
+if [ "$pkgman" = "apt" ]; then
+  $install_cmd ca-certificates curl gnupg lsb-release
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+elif [ "$pkgman" = "dnf" ]; then
+  $install_cmd dnf-plugins-core
+  sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+elif [ "$pkgman" = "pacman" ]; then
+  #$install_cmd --needed base-devel openssl zlib xz tk
+fi
 
+$update_cmd
+$install_cmd docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo apt -y update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo docker run hello-world
+sudo systemctl enable --now docker
+
+#sudo docker run hello-world
 
 echo "Installing docker... Done!"
