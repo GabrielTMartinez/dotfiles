cc5c233d146ed7b198e68996165d0b97e138ce10
GabrielTMartinez <email@email.com>
2023-02-12 19:56:34 -0300

    adjustments

--- install_all.sh
+++ install_all.sh
@@ -15,25 +15,25 @@ select_package_manager() {
   if [ $cmd_exists -eq 1 ]; then
       echo 'apt found! using it as the package manager!'
       pkgman='apt'
-      update_cmd='apt -y update; apt -y upgrade'
-      install_cmd='apt -y install'
-      autoremove_cmd='apt -y autoremove'
+      update_cmd='sudo apt -y update; sudo apt -y upgrade'
+      install_cmd='sudo apt -y install'
+      autoremove_cmd='sudo apt -y autoremove'
   else
     command_exists 'dnf'
     if [ $cmd_exists -eq 1 ]; then
       echo 'dnf found! using it as the package manager!'
       pkgman='dnf'
-      update_cmd='dnf update -y'
-      install_cmd='dnf install -y'
-      autoremove_cmd='dnf autoremove -y'
+      update_cmd='sudo dnf update -y'
+      install_cmd='sudo dnf install -y'
+      autoremove_cmd='sudo dnf autoremove -y'
     else
       command_exists 'pacman'
       if [ $cmd_exists -eq 1 ]; then
         echo 'pacman found! using it as the package manager!'
         pkgman='pacman'
-        update_cmd='pacman -Syyu --noconfirm'
-        install_cmd='pacman -S --noconfirm'
-        autoremove_cmd='pacman -Qdttq | pacman -Rs - --noconfirm'
+        update_cmd='sudo pacman -Syyu --noconfirm'
+        install_cmd='sudo pacman -S --noconfirm'
+        autoremove_cmd='sudo pacman -Qdttq | pacman -Rs - --noconfirm'
       fi
     fi
   fi
