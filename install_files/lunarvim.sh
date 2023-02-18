e4cce232f3ef99a2d0f2cd444d7fd334e99a35bc
GabrielTMartinez <email@email.com>
2023-02-17 23:29:14 -0300

    adjustments

--- install_files/lunarvim.sh
+++ install_files/lunarvim.sh
@@ -4,6 +4,11 @@ set -e
 
 echo "Installing lunarvim..."
 
+echo "Installing python..."
+if [ "$pkgman" = "dnf" ]; then
+  $install_cmd g++ libstdc++.x86_64 libstdc++-devel.x86_64 libstdc++-static.x86_64 libstdc++-docs.x86_64 libstdc++ gcc-c++-x86_64-linux-gnu.x86_64 gcc
+fi
+
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
 
 echo '
