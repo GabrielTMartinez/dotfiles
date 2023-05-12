fe52619aac0521c69ea96097cce0104f6b244f5e
GabrielTMartinez <GabrielTMartinez>
2023-05-11 23:13:41 -0300

    moving zsh config to dotfile

--- install_files/zsh.sh
+++ install_files/zsh.sh
@@ -3,70 +3,9 @@ set -e
 
 
 echo "Installing zsh..."
+
 $install_cmd zsh
 zsh --version
 
-echo '
-# keybindings
-# create a zkbd compatible hash;
-# to add other keys to this hash, see: man 5 terminfo
-typeset -g -A key
-
-key[Home]="${terminfo[khome]}"
-key[End]="${terminfo[kend]}"
-#key[Insert]="${terminfo[kich1]}"
-#key[Backspace]="${terminfo[kbs]}"
-#key[Delete]="${terminfo[kdch1]}"
-#key[Up]="${terminfo[kcuu1]}"
-#key[Down]="${terminfo[kcud1]}"
-#key[Left]="${terminfo[kcub1]}"
-#key[Right]="${terminfo[kcuf1]}"
-#key[PageUp]="${terminfo[kpp]}"
-#key[PageDown]="${terminfo[knp]}"
-#key[Shift-Tab]="${terminfo[kcbt]}"
-
-## setup key accordingly
-[[ -n "${key[Home]}"      ]] && bindkey -- "${key[Home]}"       beginning-of-line
-[[ -n "${key[End]}"       ]] && bindkey -- "${key[End]}"        end-of-line
-#[[ -n "${key[Insert]}"    ]] && bindkey -- "${key[Insert]}"     overwrite-mode
-#[[ -n "${key[Backspace]}" ]] && bindkey -- "${key[Backspace]}"  backward-delete-char
-#[[ -n "${key[Delete]}"    ]] && bindkey -- "${key[Delete]}"     delete-char
-#[[ -n "${key[Up]}"        ]] && bindkey -- "${key[Up]}"         up-line-or-history
-#[[ -n "${key[Down]}"      ]] && bindkey -- "${key[Down]}"       down-line-or-history
-#[[ -n "${key[Left]}"      ]] && bindkey -- "${key[Left]}"       backward-char
-#[[ -n "${key[Right]}"     ]] && bindkey -- "${key[Right]}"      forward-char
-#[[ -n "${key[PageUp]}"    ]] && bindkey -- "${key[PageUp]}"     beginning-of-buffer-or-history
-#[[ -n "${key[PageDown]}"  ]] && bindkey -- "${key[PageDown]}"   end-of-buffer-or-history
-#[[ -n "${key[Shift-Tab]}" ]] && bindkey -- "${key[Shift-Tab]}"  reverse-menu-complete
-#
-## Finally, make sure the terminal is in application mode, when zle is
-## active. Only then are the values from $terminfo valid.
-#if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
-#	autoload -Uz add-zle-hook-widget
-#	function zle_application_mode_start { echoti smkx }
-#	function zle_application_mode_stop { echoti rmkx }
-#	add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
-#	add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
-#fi
-
-bindkey "^[[1;5C" forward-word
-bindkey "^[[1;5D" backward-word
-bindkey '^[[3~' delete-char
-# /keybindings
-
-# basic aliases
-alias gst="git status"
-alias gpa="git pull --all"
-alias gca="git commit -am"
-alias clah="ls -lah"
-# /basic aliases
-
-HISTSIZE=5000               #How many lines of history to keep in memory
-HISTFILE=$HOME/.zsh_history #Where to save history to disk
-SAVEHIST=5000               #Number of history entries to save to disk
-#HISTDUP=erase               #Erase duplicates in the history file
-setopt    appendhistory     #Append history to the history file (no overwriting)
-setopt    sharehistory      #Share history across terminals
-setopt    incappendhistory  #Immediately append to the history file, not just when a term is killed
-' >> $HOME/.zshrc
 echo "Installing zsh... Done!"
+
