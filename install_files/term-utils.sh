e60ac28cde3780c5849423b97f41e4ba4cdde5b9
GabrielTMartinez <GabrielTMartinez>
2023-05-13 21:02:36 -0300

    p10k

--- install_files/term-utils.sh
+++ install_files/term-utils.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+
 $install_cmd alacritty neovim tmux powerline
 
 # alacritty themes
@@ -13,4 +14,9 @@ mkdir -p $HOME/.fonts/Hack/
 tar -xzvf ./fonts/hack_nerd_font.tar.gz -C $HOME/.fonts/Hack/
 fc-cache -f -v
 fc-list | grep "Hack"
+
+# powerlevel10k
+rm -rf $HOME/powerlevel10k/
+tar -xzvf ./powerlevel10k.tar.gz -C $HOME/
+ls -lah $HOME/powerlevel10k/
 
