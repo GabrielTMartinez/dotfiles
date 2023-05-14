d809b85a498fab1fdfbe83d134a57b9a21f04781
GabrielTMartinez <GabrielTMartinez>
2023-05-14 11:25:57 -0300

    removing comment vscode

--- install_files/vscode.sh
+++ install_files/vscode.sh
@@ -6,14 +6,5 @@ echo "Installing vscode..."
 
 $install_cmd code
 
-: <<'END'
-cd /tmp/
-
-curl -L -o ./vscode.rpm "https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64"
-
-sudo dnf install -y ./vscode.rpm
-
-cd -
-END
-
 echo "Installing vscode... Done!"
+
