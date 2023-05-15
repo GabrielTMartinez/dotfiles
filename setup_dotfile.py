e0feffef00a18e0c821487fa0982cfe34c734900
GabrielTMartinez <GabrielTMartinez>
2023-05-15 00:24:10 -0300

    adding setup_dotfile in python

---
+++ setup_dotfile.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+from datetime import datetime
+from pathlib import Path
+
+
+run_time = datetime.now()
+
+home = str(Path.home())
+config_folder = f"{home}/.config/"
+
+file_names = {
+    "alacritty.yml": f"{config_folder}/alacritty/",
+    "init.lua": f"{config_folder}/nvim/",
+    ".p10k.zsh": f"{home}/",
+    ".tmux.conf": f"{home}/",
+    ".zshrc": f"{home}/",
+}
+
+for file_name in file_names:
+    full_file_path = file_names[file_name] + file_name
+
+    if os.path.exists(full_file_path):
+        backup_file_name = f"{full_file_path}-{run_time}"
+        print(f"Saving a backup in: {backup_file_name}")
+        os.rename(full_file_path, backup_file_name)
+
+    print(f"Copying file ./dotfiles/{file_name} to {full_file_path}")
+    shutil.copy(f"./dotfiles/{file_name}", f"{full_file_path}")
