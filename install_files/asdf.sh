2efa11efc1b17092d0c5a71c0cde6242f9369f20
GabrielTMartinez <GabrielTMartinez>
2023-05-16 21:05:18 -0300

    adjusting asdf

--- install_files/asdf.sh
+++ install_files/asdf.sh
@@ -19,29 +19,11 @@ latest_version=$(git ls-remote --refs --tags https://github.com/asdf-vm/asdf.git
 
 git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch $latest_version
 
-echo '
-# asdf
-. "$HOME/.asdf/asdf.sh"
-# append completions to fpath
-fpath=(${ASDF_DIR}/completions $fpath)
-# initialise completions with ZSHs compinit
-autoload -Uz compinit && compinit
-# /asdf
-' >> $HOME/.zshrc
-
-echo '
-# asdf
-. "$HOME/.asdf/asdf.sh"
-. "$HOME/.asdf/completions/asdf.bash"
-# /asdf
-' >> $HOME/.bashrc
-
-. "$HOME/.asdf/asdf.sh"
-
 echo "Installing asdf... Done!"
 
 
-echo "Installing python..."
+echo "Installing python build-tools..."
+
 if [ "$pkgman" = "apt" ]; then
   $install_cmd git zlib1g-dev libffi-dev libssl-dev libbz2-dev libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev tk-dev uuid-dev libreadline-dev
 elif [ "$pkgman" = "dnf" ]; then
@@ -49,31 +31,5 @@ elif [ "$pkgman" = "dnf" ]; then
 elif [ "$pkgman" = "pacman" ]; then
   $install_cmd --needed base-devel openssl zlib xz tk
 fi
-asdf plugin add python
-asdf install python 3.11.1
-asdf global python 3.11.1
-echo "Installing python... Done!"
 
-echo "Installing rust..."
-asdf plugin add rust
-asdf install rust 1.66.0
-asdf global rust 1.66.0
-echo "Installing rust... Done!"
-
-echo "Installing nodejs..."
-asdf plugin add nodejs
-asdf install nodejs 18.12.1
-asdf global nodejs 18.12.1
-echo "Installing nodejs... Done!"
-
-#echo "Installing yarn..."
-#asdf plugin add yarn
-#asdf install yarn 1.22.19
-#asdf global yarn 1.22.19
-#echo "Installing yarn... Done!"
-
-echo "Installing java temurin..."
-asdf plugin add java
-asdf install java temurin-17.0.5+8
-asdf global java temurin-17.0.5+8
-echo "Installing java temurin... Done!"
+echo "Installing python build-tools... Done!"
