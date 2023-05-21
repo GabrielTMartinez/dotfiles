5eb44ad012bd5f3c5c26b8c117066614f850a4c6
lightwanderer <GabrielTMartinez>
2023-05-20 23:18:12 -0300

    new functionality for placing dotfiles + nvim config update

---
+++ dotfiles/.config/nvim/lua/personal/sets.lua
@@ -0,0 +1,44 @@
+-- yank to system clipboard
+vim.opt.clipboard:append("unnamedplus")
+
+vim.opt.encoding = "utf-8"
+
+-- disable bell sounds
+vim.opt.belloff = "all"
+--vim.opt.visualbell = true
+--vim.opt.t_vb = 
+
+-- turn on line numbers
+--vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- ruler and cursor positions
+--vim.opt.ruler = true
+--vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+
+-- pathogen minimalist recommendations
+--vim.cmd("syntax on")
+--vim.cmd("filetype plugin indent on")
+
+-- set tab to 4 spaces
+-- show existing tab with 4 spaces width
+vim.opt.tabstop = 4
+-- when indenting with '>', use 4 spaces width
+vim.opt.shiftwidth = 4
+-- On pressing tab, insert 4 spaces
+vim.opt.expandtab = true
+
+-- Enable backspace over auto indent etc
+vim.opt.backspace = { "indent", "eol", "start" }
+
+-- Change Explorer / Netrw style to tree
+vim.g.netrw_liststyle = 3
+
+-- Having longer updatetime (default is 4000 ms  =  4 s) leads to noticeable
+-- delays and poor user experience.
+vim.opt.updatetime = 300
+
+-- Always show the signcolumn, otherwise it would shift the text each time diagnostics appear/become resolved.
+--vim.opt.signcolumn = "yes"
+
--- setup_dotfile.py
+++ setup_dotfile.py
@@ -7,25 +7,22 @@ def setup_dotfiles():
     run_time = datetime.now()
 
     home = str(Path.home())
-    config_folder = f"{home}/.config/"
+    python_runtime_path = str(Path().resolve())
 
-    file_names = {
-        "alacritty.yml": f"{config_folder}/alacritty/",
-        "init.lua": f"{config_folder}/nvim/",
-        ".p10k.zsh": f"{home}/",
-        ".tmux.conf": f"{home}/",
-        ".zshrc": f"{home}/",
-        "config": f"{config_folder}/sway/",
-    }
+    for root, dirs, files in os.walk(f'{python_runtime_path}/dotfiles/'):
+        current_dir = root.replace(f'{python_runtime_path}/dotfiles', f'{home}')
+        
+        if not os.path.exists(current_dir):
+            print(f'Creating directory {current_dir}')
+            os.makedirs(current_dir)
 
-    for file_name in file_names:
-        full_file_path = file_names[file_name] + file_name
+        for file in files:
+            full_file_path = current_dir + '/' + file
+            if os.path.exists(full_file_path) or os.path.islink(full_file_path):
+                backup_file_name = f"{full_file_path}-{run_time}"
+                print(f"Saving a backup in: {backup_file_name}")
+                os.rename(full_file_path, backup_file_name)
 
-        if os.path.exists(full_file_path) and not os.path.islink(full_file_path):
-            backup_file_name = f"{full_file_path}-{run_time}"
-            print(f"Saving a backup in: {backup_file_name}")
-            os.rename(full_file_path, backup_file_name)
-
-        print(f"Creating symlink for ./dotfiles/{file_name} in {full_file_path}")
-        os.symlink(f'{Path().resolve()}/dotfiles/{file_name}', f'{full_file_path}')
+            print(f"Creating symlink for {root}/{file} in {full_file_path}")
+            os.symlink(f'{root}/{file}', f'{full_file_path}')
 
