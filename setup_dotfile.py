aaaaeb0496cdc3f18297e42ff6dc14cf28985339
lightwanderer <GabrielTMartinez>
2023-05-21 18:51:22 -0300

    new model for placing dotfiles

--- setup_dotfile.py
+++ setup_dotfile.py
@@ -4,25 +4,27 @@ from pathlib import Path
 
 
 def setup_dotfiles():
-    run_time = datetime.now()
+    run_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 
     home = str(Path.home())
+    home_config = f"{home}/.config"
     python_runtime_path = str(Path().resolve())
 
-    for root, dirs, files in os.walk(f'{python_runtime_path}/dotfiles/'):
-        current_dir = root.replace(f'{python_runtime_path}/dotfiles', f'{home}')
-        
-        if not os.path.exists(current_dir):
-            print(f'Creating directory {current_dir}')
-            os.makedirs(current_dir)
+    src_target_map = {
+        f"{python_runtime_path}/dotfiles/home": f"{home}/",
+        f"{python_runtime_path}/dotfiles/.config": f"{home_config}/",
+    }
 
-        for file in files:
-            full_file_path = current_dir + '/' + file
+    for src, target in src_target_map.items():
+        for file_name in os.listdir(src):
+            full_file_path = f"{target}/{file_name}"
+
             if os.path.exists(full_file_path) or os.path.islink(full_file_path):
                 backup_file_name = f"{full_file_path}-{run_time}"
                 print(f"Saving a backup in: {backup_file_name}")
                 os.rename(full_file_path, backup_file_name)
 
-            print(f"Creating symlink for {root}/{file} in {full_file_path}")
-            os.symlink(f'{root}/{file}', f'{full_file_path}')
-
+            print(
+                f"Creating symlink for {src}/{file_name} in {full_file_path}"
+            )
+            os.symlink(f"{src}/{file_name}", f"{full_file_path}")
