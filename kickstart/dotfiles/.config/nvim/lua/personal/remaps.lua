54d8fab912c4bf0dd7cf982a21829a0ed5145804
GabrielTM <GabrielTM>
2023-09-17 16:02:48 -0300

    netrw mapping

--- kickstart/dotfiles/.config/nvim/lua/personal/remaps.lua
+++ kickstart/dotfiles/.config/nvim/lua/personal/remaps.lua
@@ -3,6 +3,9 @@ vim.g.mapleader = " "
 --clear highlight
 vim.keymap.set("n", "<leader>ch", vim.cmd.noh)
 
+--Explore Tree (file tree)
+vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+
 --run black for current file
 vim.keymap.set("n", "<leader>cr", ':!black % <CR>')
 
