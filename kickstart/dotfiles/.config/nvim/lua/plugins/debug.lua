85c2eb3f03b7235801b73624616af5fa63b7d8ac
tama <tama@localhost.localdomain>
2024-03-21 22:03:25 -0300

    feat: new dap dependency

--- kickstart/dotfiles/.config/nvim/lua/plugins/debug.lua
+++ kickstart/dotfiles/.config/nvim/lua/plugins/debug.lua
@@ -11,6 +11,8 @@ return {
   'mfussenegger/nvim-dap',
   -- NOTE: And you can specify dependencies as well
   dependencies = {
+    'nvim-neotest/nvim-nio',
+
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
 
