50f979bd30bed98125db1806095fb122f62e2d2e
gabrieltmartinez <gabrieltmartinez>
2024-06-14 00:09:38 -0300

    feat: docker is optional - preferred to use podman for security (non sudo) reasons

--- kickstart/install_all.py
+++ kickstart/install_all.py
@@ -56,7 +56,7 @@ subprocess.run("eval ${update_cmd}", shell=True, check=True)
 setup_dotfile.setup_dotfiles()
 subprocess.run("./install_files/cli-utils.sh", shell=True, check=True)
 subprocess.run("./install_files/term-utils.sh", shell=True, check=True)
-subprocess.run("./install_files/docker.sh", shell=True, check=True)
+# subprocess.run("./install_files/docker.sh", shell=True, check=True)
 
 # TODO ncdu
 
