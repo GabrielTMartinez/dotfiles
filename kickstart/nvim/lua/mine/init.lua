dfe9991f2181db448cc697621a2630331c1e8815
gabrieltmartinez <gabrieltmartinez>
2025-01-05 12:55:55 -0300

    feat: rename mapping

--- kickstart/nvim/lua/mine/init.lua
+++ kickstart/nvim/lua/mine/init.lua
@@ -10,6 +10,8 @@ vim.keymap.set("n", "<leader>if", "gg0vG$=<Esc>")
 vim.keymap.set("n", "gr", vim.lsp.buf.references)
 vim.keymap.set("n", "ga", vim.lsp.buf.code_action)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+
 
 -- Omnifunc (params autocomplete)
 vim.keymap.set("i", "<C-Space><C-Space>", "<C-x><C-o>")
